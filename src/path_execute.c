475d83a279a8a20010a1799b7f5bb0114ac0349f
VM Macbook <corsica-man@hotmail.fr>
2025-03-21 13:38:15 +0100

    path execute

---
+++ src/path_execute.c
@@ -0,0 +1,51 @@
+#include "../include/minishell.h"
+
+
+char	*find_path(char *cmd, char **envp)
+{
+	char	**paths;
+	char	*path;
+	int		i;
+	char	*part_path;
+
+	i = 0;
+	while (ft_strnstr(envp[i], "PATH", 4) == 0)
+		i++;
+	paths = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (paths[i])
+	{
+		part_path = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(part_path, cmd);
+		free(part_path);
+		if (access(path, F_OK) == 0)
+			return (path);
+		free(path);
+		i++;
+	}
+	i = -1;
+	while (paths[++i])
+		free(paths[i]);
+	free(paths);
+	return (0);
+}
+
+void	execute(char **cmd, char **envp)
+{
+	int		i;
+	char	*path;
+
+	i = -1;
+	if (cmd[0] && access(cmd[0],F_OK) == 0)
+		return (path);
+	path = find_path(cmd[0], envp);
+	if (!path)
+	{
+		while (cmd[++i])
+			free(cmd[i]);
+		free(cmd);
+		error();
+	}
+	if (execve(path, cmd, envp) == -1)
+		error();
+}
